eeec09f4c8e8ad9547d2093a1428e7d8c20b5a8c
s230206025@ankarabilim.edu.tr <seninmailin@example.com>
2025-10-13 15:03:52 +0300

    feat: Pdf parser yapıldı.Pdf okuması yapıyor ve metadataları çıkartıyor

--- src/pdf_processing/pdf_parser.py
+++ src/pdf_processing/pdf_parser.py
@@ -1,15 +1,33 @@
+import os
 import pprint
 from langchain_community.document_loaders import PyPDFLoader
+import os
 
-loader = PyPDFLoader("C:/Users/goktu/OneDrive/Masaüstü/pdf_sample_file_50KB.pdf")
+pdf_path = input("PDF yolu: ").strip().strip('"')
 
-docs = loader.load()
 
-for i ,doc in enumerate(docs):
-    print(f"--- Sayfa {i + 1} ---")
-    print(doc.page_content)
-    pprint.pprint(doc.metadata)
-    print()
+print(f"📄 Girilen yol: {pdf_path}")
+print("✅ Dosya gerçekten var mı?", os.path.exists(pdf_path))
+
+if not os.path.exists(pdf_path):
+    print(f"❌ HATA: Dosya bulunamadı → {pdf_path}")
+else:
+    try:
+        loader = PyPDFLoader(pdf_path)
+
+        docs = loader.load()
+
+        for i ,doc in enumerate(docs):
+           print(f"--- Sayfa {i + 1} ---")
+           print(doc.page_content)
+           pprint.pprint(doc.metadata)
+           print()
+
+    except ValueError as e:
+     print(f"❌ HATA: PDF dosyası geçersiz veya bozuk. Detay: {e}")
+
+    except Exception as e:
+     print(f"⚠️ Beklenmeyen bir hata oluştu: {e}")
 
 
 
